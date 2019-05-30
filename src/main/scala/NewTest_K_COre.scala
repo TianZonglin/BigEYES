f6029e9d6b9666f8f7a18175398ec090f8d5df3b
1805984583@qq.com <1805984583@qq.com>
2019-05-30 13:41:47 +0800

    2019.05.30 13:41:46

--- src/main/scala/NewTest_K_COre.scala
+++ src/main/scala/NewTest_K_COre.scala
@@ -12,61 +12,63 @@ object NewTest_K_COre {
 
   def main(args: Array[String]) {
 
-    val REMOTE_JOB: Boolean = false                 // 是否提交到集群运行
-    var dbi: Double = 0d                               // 度筛选比率
-    var sbi: Double = 0d                               // 图节点采样比率
-    var fname: String = ""                          // 输入文件名
-    var input: String = ""                          // 输入文件路径
-    var output: String = ""                         // 输出文件路径
-    var tab: String = ""                            // 输入文件的文本分隔符
-    var sizeOfGraph: Long = 0                       // 输入文件的节点大小
-    var temperature: Double = 0d                       // 模拟退火温度
-    var area: Int = 0                                  // 布局大小，次方值
-    var AREA_MULTIPLICATOR: Double = 0d                // 布局除数，计算用
-    var iterations: Int = 0                            // 总迭代次数
-    var k: Double = 0d                                 // 力导向弹性系数 K
-    var epsilon: Double = 0d                           // 计算两点位置，最小量
-    var gravitys: Double =0d                        // 正作用于重力的参数
-    var REP_SCALE: Double = 0d                      // 正作用于斥力的参数
-    var ATT_SCALE: Double = 0d                      // 正作用于引力的参数
-    var speed: Double = 1d                           // 作用于 updatePos
-    var maxDisplace: Double =0d                     // 最小移动距离，作用于 updatePos
-    var SPEED_DIVISOR: Double = 0d                  // 关于移动速度，作用于 updatePos
-    val defaultNode                                    // 默认节点的绑定结构
-      = ("o", 0.0, 0.0, (0.0, 0.0, 0.0, 0.0))
+    val REMOTE_JOB: Boolean = false // 是否提交到集群运行
+    var dbi: Double = 0d // 度筛选比率
+    var sbi: Double = 0d // 图节点采样比率
+    var fname: String = "" // 输入文件名
+    var input: String = "" // 输入文件路径
+    var output: String = "" // 输出文件路径
+    var tab: String = "" // 输入文件的文本分隔符
+    var sizeOfGraph: Long = 0 // 输入文件的节点大小
+    var temperature: Double = 0d // 模拟退火温度
+    var area: Int = 0 // 布局大小，次方值
+    var AREA_MULTIPLICATOR: Double = 0d // 布局除数，计算用
+    var iterations: Int = 0 // 总迭代次数
+    var k: Double = 0d // 力导向弹性系数 K
+    var epsilon: Double = 0d // 计算两点位置，最小量
+    var gravitys: Double = 0d // 正作用于重力的参数
+    var REP_SCALE: Double = 0d // 正作用于斥力的参数
+    var ATT_SCALE: Double = 0d // 正作用于引力的参数
+    var speed: Double = 1d // 作用于 updatePos
+    var maxDisplace: Double = 0d // 最小移动距离，作用于 updatePos
+    var SPEED_DIVISOR: Double = 0d // 关于移动速度，作用于 updatePos
+    val defaultNode // 默认节点的绑定结构
+    = ("o", 0.0, 0.0, (0.0, 0.0, 0.0, 0.0))
 
     def getConf: SparkConf = {
-      if(REMOTE_JOB){
+      if (REMOTE_JOB) {
         new SparkConf()
           .setAppName("RemoteGraphX")
           .setMaster("spark://hadoop02:7077")
           .set("spark.cores.max", "20")
-          //.setJars(List("I:\\IDEA_PROJ\\VISNWK\\out\\artifacts\\visnwk_build_jar\\visnwk-build.jar"))
-      }else{
+        //.setJars(List("I:\\IDEA_PROJ\\VISNWK\\out\\artifacts\\visnwk_build_jar\\visnwk-build.jar"))
+      } else {
         new SparkConf()
           .setAppName("LocalGraphX")
           .setMaster("local")
       }
     }
+
     val sc = new SparkContext(getConf)
     sc.setCheckpointDir("checkpoint")
+
     // 必要，否则报：Checkpoint directory has not been set in the SparkContext
-    def ran:Double = {
+    def ran: Double = {
       val random = new scala.util.Random
-      random.nextDouble * 1000  - 500
+      random.nextDouble * 1000 - 500
     }
 
     def loadEdges(fn: String): Graph[Any, String] = {
       val s: String = "1.0"
       val edges: RDD[Edge[String]] =
         sc.textFile(fn)
-          .filter(l => ! l.startsWith("#") )
+          .filter(l => !l.startsWith("#"))
           //.sample(withReplacement = false, 1, salt)
           .map { //无放回
-            line =>
-              val fields = line.split(tab)
-              Edge(fields(0).toLong, fields(1).toLong, s)
-          }
+          line =>
+            val fields = line.split(tab)
+            Edge(fields(0).toLong, fields(1).toLong, s)
+        }
       val graph: Graph[Any, String] = Graph.fromEdges(edges, "defaultProperty")
       graph
     }
@@ -97,29 +99,26 @@ object NewTest_K_COre {
     }
 
 
-
-
-
-    def date:String
+    def date: String
     = new SimpleDateFormat("MM-dd_HH-mm_").format(System.currentTimeMillis())
     // 用户设定，定义输入输出，分隔符，及迭代次数，注意路径  //
     tab = "\t"
-    if(REMOTE_JOB){
+    if (REMOTE_JOB) {
       // 集群HDFS绝对路径
       fname = args(0)
-      input = "hdfs://hadoop02:9000/SNAP/DATASET/"+fname
-      output = "hdfs://hadoop02:9000/SNAP/OUTPUT/"+date+"dump_"+fname+"_"+args(1).toInt
+      input = "hdfs://hadoop02:9000/SNAP/DATASET/" + fname
+      output = "hdfs://hadoop02:9000/SNAP/OUTPUT/" + date + "dump_" + fname + "_" + args(1).toInt
       iterations = args(1).toInt
-    }else{
+    } else {
       // 本地项目相对路径
       fname = "Vote.txt"
-      input = "resources\\"+fname
-      output = "output\\"+fname
+      input = "resources\\" + fname
+      output = "output\\" + fname
       iterations = 200
     }
 
-    val graphS = loadEdges( input )
-    val cGraphS = convert( graphS ).persist()
+    val graphS = loadEdges(input)
+    val cGraphS = convert(graphS).persist()
 
 
 
@@ -127,89 +126,99 @@ object NewTest_K_COre {
     //cGraphS.degrees.collect.foreach(println(_))
 
 
-  //////////////////////////////////////////////////////////////// 二跳邻居
+    //////////////////////////////////////////////////////////////// 二跳邻居
 
 
-   //type VMap=Map[VertexId,Int]
+    //type VMap=Map[VertexId,Int]
 
-   ///**
-   //  * 节点数据的更新 就是集合的union
-   //  */
-   //def vprog(vid:VertexId,vdata:VMap,message:VMap)
-   //:Map[VertexId,Int]=addMaps(vdata,message)
+    ///**
+    //  * 节点数据的更新 就是集合的union
+    //  */
+    //def vprog(vid:VertexId,vdata:VMap,message:VMap)
+    //:Map[VertexId,Int]=addMaps(vdata,message)
 
-   ///**
-   //  * 发送消息
-   //  */
-   //def sendMsg(e:EdgeTriplet[VMap, _])={
+    ///**
+    //  * 发送消息
+    //  */
+    //def sendMsg(e:EdgeTriplet[VMap, _])={
 
-   //  //取两个集合的差集  然后将生命值减1
-   //  val srcMap=(e.dstAttr.keySet -- e.srcAttr.keySet).map { k => k->(e.dstAttr(k)-1) }.toMap
-   //  val dstMap=(e.srcAttr.keySet -- e.dstAttr.keySet).map { k => k->(e.srcAttr(k)-1) }.toMap
+    //  //取两个集合的差集  然后将生命值减1
+    //  val srcMap=(e.dstAttr.keySet -- e.srcAttr.keySet).map { k => k->(e.dstAttr(k)-1) }.toMap
+    //  val dstMap=(e.srcAttr.keySet -- e.dstAttr.keySet).map { k => k->(e.srcAttr(k)-1) }.toMap
 
-   //  if(srcMap.size==0 && dstMap.size==0)
-   //    Iterator.empty
-   //  else
-   //    Iterator((e.dstId,dstMap),(e.srcId,srcMap))
-   //}
+    //  if(srcMap.size==0 && dstMap.size==0)
+    //    Iterator.empty
+    //  else
+    //    Iterator((e.dstId,dstMap),(e.srcId,srcMap))
+    //}
 
-   ///**
-   //  * 消息的合并
-   //  */
-   //def addMaps(spmap1: VMap, spmap2: VMap): VMap =
+    ///**
+    //  * 消息的合并
+    //  */
+    //def addMaps(spmap1: VMap, spmap2: VMap): VMap =
     //  (spmap1.keySet ++ spmap2.keySet).map {
-   //    k => k -> math.min(spmap1.getOrElse(k, Int.MaxValue), spmap2.getOrElse(k, Int.MaxValue))
-   //  }.toMap
+    //    k => k -> math.min(spmap1.getOrElse(k, Int.MaxValue), spmap2.getOrElse(k, Int.MaxValue))
+    //  }.toMap
 
 
-   //val two=2  //这里是二跳邻居 所以只需要定义为2即可
-   //val newG=cGraphS.mapVertices((vid,_)=>Map[VertexId,Int](vid->two))
-   //  .pregel(Map[VertexId,Int](), two, EdgeDirection.Out)(vprog, sendMsg, addMaps)
-   ////newG.vertices.collect().foreach(println(_))
-   ////过滤得到二跳邻居 就是value=0 的顶点
-   //val twoJumpFirends=newG.vertices
-   //  .mapValues(_.filter(_._2==0).keys)
-
-   //twoJumpFirends.collect().foreach(println(_))
+    //val two=2  //这里是二跳邻居 所以只需要定义为2即可
+    //val newG=cGraphS.mapVertices((vid,_)=>Map[VertexId,Int](vid->two))
+    //  .pregel(Map[VertexId,Int](), two, EdgeDirection.Out)(vprog, sendMsg, addMaps)
+    ////newG.vertices.collect().foreach(println(_))
+    ////过滤得到二跳邻居 就是value=0 的顶点
+    //val twoJumpFirends=newG.vertices
+    //  .mapValues(_.filter(_._2==0).keys)
 
     //twoJumpFirends.collect().foreach(println(_))
 
-    val diameter = 7
+    //twoJumpFirends.collect().foreach(println(_))
 
-    val distribution = cGraphS.degrees.map(t => (t._2,t._1+"")).
-      reduceByKey(_+","+_).
-      sortBy(_._1,false).collect()
 
+    //===================================================================
+    //获取超级节点
+    val distribution = cGraphS.degrees.map(t => (t._2, t._1 + "")).
+      reduceByKey(_ + "," + _).
+      sortBy(_._1, false).collect()
     val counts = distribution.length
-
     val theta = (counts * 0.2).toInt
-
     val head_d = distribution.take(theta) //取前百分20
-
-    val head_nodes = head_d.reduce((a,b)=>(1,a._2+","+b._2))
-
-    //println("MAX_ID："+dufenbu.max()._1)
-
-    //println("MAX_DGREE："+dufenbu.max()._2)
-
-
-
-
-
-
-    val G = KCore.run(cGraphS, 2, 1)//达到最大次数（对于无法保证收敛的算法）或无消息传递时结束
-
+    val d_max = head_d.take(1)(0)._1 //最大出入度
+    val head_nodes = head_d.reduce((a, b) => (1, a._2 + "," + b._2))._2.split(",")
+    println("> Get V_sup " + d_max)
+    head_nodes.foreach(println)
+    //打印抽取的节点
+    //===================================================================
+    //Core分层
+    val diameter = 7
+    val level = (diameter / 2).toInt
+    val span = (d_max / level).toInt
+    println(s"counts:${head_nodes.length},theta:$theta,d_max:$d_max,diameter:$diameter,level:$level,span:$span")
+    var index = d_max
+    while (index > 0) {
+      index = index - span
+      //println(index) //各层的指定Core的K值
+      //TO DO
+      //如何计算SHELL
+    }
+    var i = 30
+    while (i < 100){
+      val testNUM = i
+      val G = KCore.run(cGraphS, testNUM, 1)
+      //达到最大次数（对于无法保证收敛的算法）或无消息传递时结束
+      val CoreYES = G.vertices.map(t => (t._2, t._1)).groupByKey.map(t => (t._1, t._2.size)).sortBy(_._1, false).take(1)(0)._2
+      println(s"$testNUM\t$CoreYES")
+      i += 5
+    }
 
 
     println("> DONE!")
-    println("> DONE!")
-    //head_nodes.foreach(println)
-    println(head_nodes)
-   // val G = KCore.run(cGraphS, 2, 10)
 
-   // val G = KCore.run(cGraphS, 2, 10)
+    //println(head_nodes)
+    // val G = KCore.run(cGraphS, 2, 10)
 
-   // G.vertices.foreach(println)
+    // val G = KCore.run(cGraphS, 2, 10)
+
+    // G.vertices.foreach(println)
 
 
 

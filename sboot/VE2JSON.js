cf332756741d59d6c14daed8c97d688a3ccfed65
1805984583@qq.com <1805984583@qq.com>
2019-06-28 21:00:00 +0800

    2019.06.28 21:00:00

--- sboot/VE2JSON.js
+++ sboot/VE2JSON.js
@@ -18,8 +18,8 @@ for(let index = 1; index <=5; index++){
         strALL += fs.readFileSync("I:\\IDEA_PROJ\\Visualization\\output\\OUTPUT\\" + filename + "\\part-00001", "utf8");
 
 
-        let nodes = {};
-        let links = {};
+        let nodes = [];
+        let links = [];
         if(TYPE[k] == "Vertices") {
 
             let lines = strALL.split("\n");
@@ -31,7 +31,7 @@ for(let index = 1; index <=5; index++){
                 nd.value = "1";
                 nd.cx = eachline[2];
                 nd.cy = eachline[3];
-                nodes.push(nd)
+                nodes.push(nd);
                 //toFIle += '{"weight": "0","name+": "o","value": "1", "cx":"0","cy": "0"}';
             }
 
@@ -41,20 +41,34 @@ for(let index = 1; index <=5; index++){
                 let eachline = lines[t].split(",");
                 let v1 = eachline[0].replace("Edge(");
                 let v2 = eachline[1];
-                let node1 = nodes.find(function (obj){ if(obj.name== v1){ return obj; } });
-                let node2 = nodes.find(function (obj){ if(obj.name== v2){ return obj; } });
-                let lk = {};
-                lk.value = "1";
-                lk.cx = node1.cx
-                lk.cy = eachline[3];
-                links.push(lk)
+                let node1 = nodes.find(function (obj){
+                    console.log(obj.name +"--------------"+ v1);
+                    if(obj.name == v1){
+                        return obj;
+                    }else{
+                        return null;}
+                });
+                let node2 = nodes.find(function (obj){ if(obj.name == v2){return obj; }else{ return null;} });
+                if(node1 != null && node2 != null){
+                    let lk = {};
+                    lk.value = "1";
+                    lk.x1 = node1.cx;
+                    lk.y1 = node1.cy;
+                    lk.x2 = node2.cx;
+                    lk.x2 = node2.cy;
+                    links.push(lk);
+                }
+
                 //"{"value": "0","x1": "${x.srcAttr._2}","y1": "${x.srcAttr._3}","x2": "${x.dstAttr._2}","y2": "${x.dstAttr._3}"},"
             }
         }
+        let whole = {
+            "nodes": nodes,
+            "links": links
+        }
 
 
-
-        let toFIle = ""
+        let toFIle = JSON.stringify(whole);
         fs.writeFile("I:\\IDEA_PROJ\\Visualization\\output\\OUTPUT\\" + filename + ".json",
             toFIle, {'flag': 'a'}, function (err) {
             if (err) {
@@ -69,15 +83,3 @@ for(let index = 1; index <=5; index++){
 }
 
 
-
-p => {
-    p.println("""{"nodes": [""")
-    g.vertices.collect.foreach(
-        x => p.println(s"""{"weight": "0","name": "o","value": "${x._2._4._3/10f}","cx":"${x._2._2}","cy": "${x._2._3}"}, """)
-)
-    p.println("""{"weight": "0","name": "o","value": "0","cx":"0","cy": "0"}],    "links": [""")
-    g.triplets.collect.foreach(
-        x => p.println(s"""{"value": "0","x1": "${x.srcAttr._2}","y1": "${x.srcAttr._3}","x2": "${x.dstAttr._2}","y2": "${x.dstAttr._3}"},""")
-)
-    p.println("""{"value": "0","x1": "0","y1": "0","x2": "0","y2": "0"}]}""")
-}
